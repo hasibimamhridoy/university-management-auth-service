c8ca6ee6383f54d2e286466148e06b62a30a9f40
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-10-15 02:03:03 +0600

    husky testing

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,16 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express'
 
-import cors from "cors"; 
+import cors from 'cors'
 
+const app: Application = express()
+const port = 3000
 
-const app : Application = express();
-const port = 3000;
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded());
+app.get('/', (req: Request, res: any) => {
+  res.send('Woking fine')
+})
 
-app.get("/", (req : Request, res : Response) => {
-  res.send("Woking fine");
-});
-
-
-export default app ;
-
-
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log('Database connection established');
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connection established')
     app.listen(process.env.PORT, () => {
-        console.log(`Example app listening on port ${config.port}`);
-      });
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log('Failed to connect database: ' + error);
+    console.log('Failed to connect database: ' + error)
   }
 }
 
-main();
+main()
