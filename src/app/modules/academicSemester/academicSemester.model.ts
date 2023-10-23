f2dfb0092662fe66a0e3ff91fdd5ef1678d55e2d
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-10-23 16:21:01 +0600

    Same year semester validation using mongoose pre hook.

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
+import status from 'http-status'
+
 import { IAcademicSemester, AcademicSemesterModel } from './academicSemester.interface'
 import {
   AcademicSemesterCodes,
   AcademicSemesterTitles,
   AcademicSemesterMonths
 } from './academicSemester.constant'
+import ApiError from '../../../error/ApiError'
 
 const { Schema } = mongoose
 
@@ -37,6 +40,17 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
   },
   { timestamps: true }
 )
+
+academicSemesterSchema.pre('save', async function (next) {
+  const query = { title: this.title, year: this.year }
+  const isExits = await AcademicSemester.findOne(query)
+
+  if (isExits) {
+    throw new ApiError(status.CONFLICT, 'Academic Semester is alrady exits in this year')
+  } else {
+    next()
+  }
+})
 
 export const AcademicSemester = mongoose.model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
